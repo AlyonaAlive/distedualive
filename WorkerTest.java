b38cd17a3ea3927b8a35c718d2725e0c22750e17
denisweeks <denisweeks@gmail.com>
2015-04-21 13:49:19 +0400

    Update WorkerTest.java

--- WorkerTest.java
+++ WorkerTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.Assert.*;
 import org.junit.Test;
 
@@ -11,14 +10,13 @@ public class WorkerTest {
     public void testAdd() throws InterruptedException {
 		Data    d = new Data();
 
-        Worker w3=new Worker(3, d);
+        //Worker w3=new Worker(3, d);
         Worker w2=new Worker(2, d);
-        Worker w1=new Worker(1, d);
+        //Worker w1=new Worker(1, d);
 
         w2.join();
         String s = "Tic-Tak-Toy";
-              
-        assertEquals("nullTic-Tak-Toy", d.testString);
+        assertEquals("nullTic-Tak-Toy", d.testString); // Альона, будь ласка! Виправ це!
       
     }
 
